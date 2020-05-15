327458c945333c53d36364b121611d695b611449
igornfaustino <igornfaustino@gmail.com>
2020-05-15 19:01:45 +0100

    [feature] add timer control

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import './App.css';
+import Timer from './components/Timer';
 
 function App() {
+  const [speed, setSpeed] = useState(1);
+  const [playing, setPlaying] = useState(false);
+  const [initialTimeSeconds, setInitialTimeSeconds] = useState(0);
+  const [currentTimeSeconds, setCurrentTimeSeconds] = useState(0);
+
+  const handleInput = useCallback((e) => {
+    const timeInMinutes = parseInt(e.target.value);
+    const timeInSeconds = timeInMinutes * 60;
+    setInitialTimeSeconds(timeInSeconds);
+    setCurrentTimeSeconds(timeInSeconds);
+  }, []);
+
+  const handlePause = useCallback(() => {
+    setPlaying((prev) => !prev);
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setCurrentTimeSeconds(initialTimeSeconds);
+    setPlaying(true);
+  }, [initialTimeSeconds]);
+
+  const intervalMs = useMemo(() => 1000 / speed, [speed]);
+
+  const playDisabled = useMemo(() => currentTimeSeconds <= 0, [
+    currentTimeSeconds,
+  ]);
+
+  const playButton = useMemo(() => {
+    if (playing)
+      return (
+        <button id="playBtn" onClick={handlePause}>
+          Pause
+        </button>
+      );
+
+    return (
+      <button id="playBtn" onClick={handlePause} disabled={playDisabled}>
+        Play
+      </button>
+    );
+  }, [handlePause, playDisabled, playing]);
+
+  const resetButton = useMemo(
+    () => (
+      <button
+        onClick={handleReset}
+        disabled={currentTimeSeconds === initialTimeSeconds}
+      >
+        Reset
+      </button>
+    ),
+    [currentTimeSeconds, handleReset, initialTimeSeconds]
+  );
+
+  const speedButtons = useMemo(() => {
+    return (
+      <>
+        <button
+          id="btn1x"
+          onClick={() => setSpeed(1)}
+          className={speed === 1 && 'selected'}
+        >
+          1x
+        </button>
+        <button
+          id="btn1_5x"
+          onClick={() => setSpeed(1.5)}
+          className={speed === 1.5 && 'selected'}
+        >
+          1.5x
+        </button>
+        <button
+          id="btn2x"
+          onClick={() => setSpeed(2)}
+          className={speed === 2 && 'selected'}
+        >
+          2x
+        </button>
+      </>
+    );
+  }, [speed]);
+
+  useEffect(() => {
+    if (!playing) return;
+    const interval = setInterval(() => {
+      setCurrentTimeSeconds((prev) => {
+        const newTime = prev - 1 > 0 ? prev - 1 : 0;
+        if (newTime <= 0) clearInterval(interval);
+        return newTime;
+      });
+    }, intervalMs);
+    return () => clearInterval(interval);
+  }, [intervalMs, playing]);
+
+  useEffect(() => {
+    if (currentTimeSeconds <= 0) setPlaying(false);
+  }, [currentTimeSeconds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for="minutes">Countdown: </label>
+      <input
+        id="minutes"
+        placeholder="(min)"
+        onChange={handleInput}
+        type="number"
+        value={initialTimeSeconds / 60}
+        min={0}
+        max={59}
+      />
+      <Timer
+        currentTimeSeconds={currentTimeSeconds}
+        initialTimeSeconds={initialTimeSeconds}
+      />
+      {playButton}
+      {resetButton}
+      {speedButtons}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App /> works properly', () => {
+  const getWrapper = () => shallow(<App />);
+
+  test('play button', () => {
+    const wrapper = getWrapper();
+    wrapper.find('#playBtn').simulate('click');
+    expect(wrapper.find('#playBtn').text()).toBe('Pause');
+  });
+
+  test('speed button 1x', () => {
+    const wrapper = getWrapper();
+    wrapper.find('#btn1x').simulate('click');
+    expect(wrapper.find('#btn1x').find('.selected')).toHaveLength(1);
+    expect(wrapper.find('#btn1_5x').find('.selected')).toHaveLength(0);
+    expect(wrapper.find('#btn2x').find('.selected')).toHaveLength(0);
+  });
+
+  test('speed button 1.5x', () => {
+    const wrapper = getWrapper();
+    wrapper.find('#btn1_5x').simulate('click');
+    expect(wrapper.find('#btn1x').find('.selected')).toHaveLength(0);
+    expect(wrapper.find('#btn1_5x').find('.selected')).toHaveLength(1);
+    expect(wrapper.find('#btn2x').find('.selected')).toHaveLength(0);
+  });
+
+  test('speed button 2x', () => {
+    const wrapper = getWrapper();
+    wrapper.find('#btn2x').simulate('click');
+    expect(wrapper.find('#btn1x').find('.selected')).toHaveLength(0);
+    expect(wrapper.find('#btn1_5x').find('.selected')).toHaveLength(0);
+    expect(wrapper.find('#btn2x').find('.selected')).toHaveLength(1);
+  });
+
+  test('minutes input', () => {
+    const wrapper = getWrapper();
+    wrapper.find('#minutes').simulate('change', { target: { value: 2 } });
+    expect(wrapper.find('#minutes').prop('value')).toBe(2);
+    expect(wrapper.find('Timer').prop('initialTimeSeconds')).toBe(120);
+  });
 });
